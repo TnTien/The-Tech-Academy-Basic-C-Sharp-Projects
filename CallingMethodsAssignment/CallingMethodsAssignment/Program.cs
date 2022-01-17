d6e7c79bd838bbb2ae3d89f38e22cc080acb3207
TnTien <nguyen.tien25@gmail.com>
2022-01-17 15:27:50 -0700

    Calling Methods Assignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  instantiates object to be able to call methods
+            mathOperation mathoperation = new mathOperation();
+
+            //  prints message on console
+            Console.WriteLine("Choose a number for math operations.");
+            //  converts user input and saves to int var
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            //  displays the results of each method using the argument provided by the user input.
+            Console.WriteLine("Half of " + number + " is " + mathoperation.Half(number) + ".");
+            Console.WriteLine(mathoperation.Double(number) + " is double of " + number + ".");
+            Console.WriteLine(mathoperation.Triple(number) + " is triple of " + number + ".");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/mathOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    class mathOperation
+    {
+        //  methods for class mathOperation
+        //  takes in an int and returns an int
+        public int Half(int num) 
+        {
+            //  returns num / 2
+            return num / 2;
+        }
+        
+        public int Double(int num)
+        {
+            //  returns num * 2
+            return num * 2;
+        }
+        public int Triple(int num)
+        {
+            //  retunrs num * 3
+            return num * 3;
+        }
+
+    }
+}
