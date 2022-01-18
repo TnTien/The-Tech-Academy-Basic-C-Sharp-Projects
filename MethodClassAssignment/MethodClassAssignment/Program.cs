c7b4e5c728bd2e8b847228c8042c5960517e93b5
TnTien <nguyen.tien25@gmail.com>
2022-01-17 18:27:20 -0700

    Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  instantiates class
+            operations operations = new operations();
+
+            //  calls method, passing 2 numbers
+            operations.Add(10, 20);
+            //  calls method, specifies parameters by name
+            operations.Add(num2: 5, num: 2);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/operations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class operations
+    {
+        public void Add(int num, int num2)
+        {
+            int numTotal = num + num2;
+            //  prints num2 on console
+            Console.WriteLine(num2);
+        }
+    }
+}
