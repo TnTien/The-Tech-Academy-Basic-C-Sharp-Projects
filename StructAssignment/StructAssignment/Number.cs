47d56980cdb19f452b44eaa442826054c9832f29
TnTien <nguyen.tien25@gmail.com>
2022-01-23 16:53:21 -0700

    Struct Assignment

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    //  structure data type
+    public struct Number
+    {
+        //  property with data type of decimal
+        public decimal Amount { get; set; }
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  instantiates object of Number
+            Number number = new Number();
+            //  assign amount to number
+            number.Amount = 12.00M;
+
+            //  prints to console
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
