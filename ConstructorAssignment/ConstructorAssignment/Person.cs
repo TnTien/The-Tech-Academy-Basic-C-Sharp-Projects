03d56a27db7b8e4551936285d50dde9dd49fe4de
TnTien <nguyen.tien25@gmail.com>
2022-01-27 22:49:56 -0700

    Constructor Assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Person
+    {
+        public string Name;
+        public string Address;
+        public string Postcode;
+        //  constructor with one parameter, this will inherit from the other constructor
+        //  if only one argument is entered, the other parameters will be entered as string "Blank"
+        public Person(string name) : this(name, "Blank", "Blank")
+        {
+        }
+        //  constructor with three parameter
+        public Person(string name, string address, string postcode)
+        {
+            Name = name;
+            Address = address;
+            Postcode = postcode;
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  const variable
+            const string  con= "constant";
+            //  implicitly typed var
+            var newList = new List<string>();
+        }
+    }
+}
