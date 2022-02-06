523c7c918f7dfb79d3c7a694bb88c54f8dc9c76d
TnTien <nguyen.tien25@gmail.com>
2022-02-06 15:54:23 -0700

    Final Challenge Assignment

---
+++ CodeFirstFinalChallengeAssignment/CodeFirstFinalChallengeAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeFirstFinalChallengeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
